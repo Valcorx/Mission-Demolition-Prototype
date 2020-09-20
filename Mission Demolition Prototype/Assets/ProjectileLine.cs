16e2dbbfb53262a30a20435121149ead861e1eea
Leo <lnikonowicz@yahoo.com>
2020-09-19 22:15:47 -0500

    Fixed camera and worked on projectileLine
    Fixed camera and worked on projectileLine

---
+++ Mission Demolition Prototype/Assets/ProjectileLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLine : MonoBehaviour
+{
+    static public ProjectileLine S;
+
+    [Header("Set in Inspector")]
+    public float minDist = 0.1f;
+
+    private LineRenderer line;
+}
