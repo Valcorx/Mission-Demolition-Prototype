192603f6617223fcc785336ffe89d8e253bcb15d
Leo <lnikonowicz@yahoo.com>
2020-09-20 22:17:26 -0500

    Finished the Line and goal scripts
    Finished the Line and goal scripts

--- Mission Demolition Prototype/Assets/Slingshot.cs
+++ Mission Demolition Prototype/Assets/Slingshot.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -17,8 +20,18 @@ public class Slingshot : MonoBehaviour
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
